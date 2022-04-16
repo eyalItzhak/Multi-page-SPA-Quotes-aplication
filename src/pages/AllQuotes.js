73fb12756b897fd5ddedd2a393c59e60e23a379c
eyalItzhak <62293316+eyalItzhak@users.noreply.github.com>
2022-04-17 00:06:11 +0300

    add quote form

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,13 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  {id : 'q1',author :'Winston Churchill',text:'Vengeance is the most costly and dissipating of luxuries'},
+  {id : 'q2',author :'Sun Tzu',text:'If you know the enemy and know yourself, you need not fear the result of a hundred battles.'},
+
+]
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>
+  return <QuoteList quotes={DUMMY_QUOTES}/>
 };
 
 export default AllQuotes;
