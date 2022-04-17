ccaf1d4c438c69d7a4d7a8fd37401d0d614056f3
eyalItzhak <62293316+eyalItzhak@users.noreply.github.com>
2022-04-17 13:14:21 +0300

    .

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,15 +1,30 @@
 import { Fragment } from 'react';
 import { useParams,Route } from 'react-router-dom';
 
+import HighLightedQuote from '../components/quotes/HighlightedQuote'
+
 import Comments from "../components/comments/Comments"
+
+
+const DUMMY_QUOTES = [
+  {id : 'q1',author :'Winston Churchill',text:'Vengeance is the most costly and dissipating of luxuries'},
+  {id : 'q2',author :'Sun Tzu',text:'If you know the enemy and know yourself, you need not fear the result of a hundred battles.'},
+
+]
 
 const QuoteDetail = () => {
   const params = useParams();
 
+
+  const quote = DUMMY_QUOTES.find (quote =>quote.id===params.quoteId)
+
+  if(!quote){
+    return <p>No quote found</p>
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighLightedQuote text={quote.text} author={quote.author}/>
       <Route path={`/quotes/${params.quoteId}/comments`}>
       <Comments/>
 
